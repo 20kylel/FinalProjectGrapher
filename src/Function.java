4e54b61eed64bb01e3facf9466a1a6ee4e9df383
20matthewji <20matthewji@students.harker.org>
2017-05-25 16:48:42 -0700

    Starting Function Class

--- src/Function.java
+++ src/Function.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 
 public class Function {
 
+	private String function;
+	
+	public Function(String f){
+		function = f;
+	}
+	
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,4 +1,8 @@
 
 public class Graph {
 
+	public static void main(String[] args){
+		
+	}
+	
 }
