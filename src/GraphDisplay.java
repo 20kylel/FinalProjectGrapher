cb37208c271c6a38e91d8757b5a176ee135da05b
20kylel <20kylel@students.harker.org>
2017-05-25 16:20:18 -0700

    Fix the Display
    Fixed it.

--- src/GraphDisplay.java
+++ src/GraphDisplay.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -5,6 +6,7 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * this works
@@ -15,18 +17,26 @@ public class GraphDisplay extends JComponent{
 	public GraphDisplay(int x, int y, int width, int height){
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(this);
 		frame.pack();
 		frame.setVisible(true);
 		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 	}
 	
 	public static void main(String[] args){
-		GraphDisplay gd = new GraphDisplay(0,0,100, 100);
+		GraphDisplay gd = new GraphDisplay(0,0,1000, 1000);
+		
 	}
 	
 	protected void paintComponent(Graphics g)
 	{
+		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D)g;
 		g2.drawImage(image, 0,0,null );
+		
 	}
+	
+	public Dimension getPreferredSize() {
+        return new Dimension(400, 400);
+    }
 }
