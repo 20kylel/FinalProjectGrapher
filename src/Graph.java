479734cbbeaaa0b1ff2ea8d4ac671cf3db76dd56
HenryW <20henryw@students.harker.org>
2017-05-25 18:09:51 -0700

    Revised Graph Changes
    plotGraph() works... theoretically

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,40 @@
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 public class Graph {
 
-	public static void main(String[] args){
-		
+		private ArrayList<Function> functions;
+		private GraphDetails details;
+		private GraphDisplay display;
+
+		public Graph(GraphDetails details)
+		{
+			functions = new ArrayList<>();
+			this.details = details;
+			display = details.getDisplay();
+		}
+
+		public void addFunction(Function function)
+		{
+			functions.add(function);
+		}
+
+		public void setPoint(double x, double y)
+		{
+			display.image.setRGB((int) (x + details.getOriginX()), (int) (y + details.getOriginY()), 255);
+		}
+
+		public void plotGraph() {
+			for (Function f : functions) {
+				for (int x = (int) details.getxMin(); x < details.getxMax(); x++) {
+					for (int inc = 0; inc < (int) details.getInputInc(); inc++)
+					{
+						setPoint(x, f.evaluate());
+					}
+				}
+			}
+		}
+
+
 	}
-	
-}
+
