924f39761750e86eb1a0e7025baa4a561b14e58a
20matthewji <20matthewji@students.harker.org>
2017-05-30 12:57:56 -0700

    Function changes

--- src/Function.java
+++ src/Function.java
@@ -1,61 +1,51 @@
+import java.util.HashMap;
 
 public class Function {
 
 	private String function;
-	private String validChars[] = {"+", "-", "*", "/", "^", "%", 
-			"x", "y", "X", "Y", "=", "(", ")",
-			"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	private HashMap<String, String> valid;
 
 	public Function(String f){
-		function = validFunction(f);
+		valid.put("+", "+");
+		valid.put("-", "-");
+		valid.put("*", "*");
+		valid.put("/", "/");
+		valid.put("^", "^");
+		valid.put("%", "%");
+		valid.put("x", "x");
+		valid.put("X", "X");
+		valid.put("(", "(");
+		valid.put(")", ")");
+		for(int i=0; i<10; i++){
+			valid.put(Integer.toString(i), Integer.toString(i));
+		}
+		
+		if(validFunction(f)){
+			function = f;
+		} else {
+			System.out.println("Invalid function: " + f);
+		}
 	}
 
-	private String validFunction(String f){
-		String s = "";
+	private boolean validFunction(String f){
 		for(int i=0; i<f.length(); i++){
-			if(isValidChar(f.substring(i, i+1))){
-				s += f.substring(i, i+1);
+			String s = f.substring(i, i+1);
+			if(valid.get(s)==null){
+				return false;
 			}
 		}
-		return s;
+		return true;
 	}
 
-	/*
-	 * Invalid if:
-	 * Uneven parenthesis
-	 * Doubling operators (+, -, *, /, ^, %)
-	 * Doubling variables
-	 * Not starting with "y="
-	 */
-	public boolean isValid(String f){
-		return false;
+	public double evaluate(int x){
+		
 	}
-
-	private boolean isValidChar(String s){
-		for(int i=0; i<validChars.length; i++){
-			if(s.equals(validChars[i])){
-				return true;
-			}
-		}
-		return false;
-	}
-
+	
 	public String getFunction(){ return function; }
 
 	public static void main(String[] args){
 		Function f = new Function("y=x");
 		System.out.println(f.getFunction());
 	}
-
-	public double evaluate(int x){
-		return calculate(function, x);
-	}
 	
-	private double calculate(String function, int x){
-		double val = 0;
-		for(int i=0; i<function.length(); i++){
-			
-		}
-		return val;
-	}
 }
