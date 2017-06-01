eed4ec4212ba5c7e8bcd7d5c7258734f75b31271
20matthewji <20matthewji@students.harker.org>
2017-06-01 13:17:34 -0700

    Moving Graph background
    Still need to get the actual equation to move correctly

--- src/Graph.java
+++ src/Graph.java
@@ -1,21 +1,25 @@
+import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Graph extends KeyAdapter {
 
 	private ArrayList<Function> functions;
 	private GraphDetails details;
-	private GraphDisplay2 display;
+	private static GraphDisplay2 display;
 	private double scale;
 
 	public Graph(GraphDetails details, double scale)
 	{
 		functions = new ArrayList<>();
 		this.details = details;
-		display = details.getDisplay();
+		display = new GraphDisplay2(this, details);
 		this.scale = scale;
+		
+		display.addKeyListener(this);
 	}
 
 	public void addFunction(Function function)
@@ -55,10 +59,71 @@ public class Graph extends KeyAdapter {
 		}
 	}
 	*/
-		
+	
 	public void keyPressed(KeyEvent e){
-		
+		if(e.getKeyCode()==KeyEvent.VK_DOWN){
+			display.shiftY(1);
+		} else if(e.getKeyCode()==KeyEvent.VK_UP){
+			display.shiftY(-1);
+		} else if(e.getKeyCode()==KeyEvent.VK_LEFT){
+			display.shiftX(-1);
+		} else if(e.getKeyCode()==KeyEvent.VK_RIGHT){
+			display.shiftX(1);
+		} else if(e.getKeyCode()==KeyEvent.VK_0){
+			display.shiftX(-display.getXShift());
+			display.shiftY(-display.getYShift());
+		}
+		display.repaint();
 	}
+	
+	public static void main(String[] args){
+		GraphDetails details = new GraphDetails(null);
+		Graph graph = new Graph(details, 1);
+		
+		details.setDisplay(display);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter functions below: ");
+		System.out.print("f(x)=");
+
+		while(true){
+			if(scanner.hasNext()){
+				String function = scanner.nextLine();
+				Function f = new Function(function, getColor(scanner));
+				graph.addFunction(f);
+
+				System.out.print("f(x)=");
+			}
+			display.repaint();
+		}
+
+	}
+
+	private static Color getColor(Scanner scanner){
+		System.out.print("Color (red, orange, yellow, green, blue, purple, black): ");
+		String color = scanner.nextLine().toLowerCase();
+
+		if(color.equals("red")){
+			return Color.RED;
+		} else if(color.equals("orange")){
+			return Color.ORANGE;
+		} else if(color.equals("yellow")){
+			return Color.YELLOW;
+		} else if(color.equals("green")){
+			return Color.GREEN;
+		} else if(color.equals("blue")){
+			return Color.BLUE;
+		} else if(color.equals("purple")){
+			return Color.MAGENTA;
+		} else if(color.equals("black")){
+			return Color.BLACK;
+		} else {
+			System.out.println("Invalid color; Default color (Black) chosen");
+			return Color.BLACK;
+		}
+	}
+	
 
 }
 
--- src/GraphDisplay2.java
+++ src/GraphDisplay2.java
@@ -1,3 +1,4 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -13,30 +14,36 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class GraphDisplay2 extends JComponent {
-	
+
 	private JFrame frame;
 	private Color color;
 	private Graph graph;
 	private GraphDetails gd;
 
+	private int xshift;
+	private int yshift;
+
 	public GraphDisplay2(Graph graph, GraphDetails gd)
 	{	
 		this.graph = graph;
 		this.gd = gd;
-		
+
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setPreferredSize(new Dimension(500, 500));
+		setPreferredSize(new Dimension(600, 600));
 		frame.getContentPane().add(this);
 
 		frame.setTitle("Grapher");
 		frame.setResizable(false);
-		
+
 		frame.pack();
 		frame.setVisible(true);
-		
+
 		color = Color.WHITE;
+
+		xshift = 0;
+		yshift = 0;
 	}
 
 	public void setTitle(String title)
@@ -48,44 +55,64 @@ public class GraphDisplay2 extends JComponent {
 	{
 		this.color = color;
 	}
-	
+
 	protected void paintComponent(Graphics g)
 	{
+		int xscale = (int) (getWidth()/(gd.getxMax()-gd.getxMin()));
+		int yscale = (int) (getHeight()/(gd.getyMax()-gd.getyMin()));
+
 		Graphics2D g2 = (Graphics2D)g;
 		g2.setColor(Color.WHITE);
 		g2.fill(new Rectangle2D.Double(0, 0, getWidth(), getHeight()));
-		
+
 		g2.setColor(Color.GRAY);
-		g2.fillRect(0, getHeight()/2-1, getWidth(), 3);
-		g2.fillRect(getWidth()/2-1, 0, 3, getHeight());
-		
-		int xscale = (int) (getWidth()/(gd.getxMax()-gd.getxMin()));
-		int yscale = (int) (getHeight()/(gd.getyMax()-gd.getyMin()));
-		
-		//Adds tick marks
+		double xAxis = getWidth()/2+xshift*xscale*gd.getxInc();
+		double yAxis = getHeight()/2+yshift*yscale*gd.getyInc();
+		g2.fillRect(0, (int) (yAxis-1), getWidth(), 3);
+		g2.fillRect((int) (xAxis-1), 0, 3, getHeight());
+
 		g2.setColor(Color.LIGHT_GRAY);
-		for(int i=0; i<getWidth(); i+=xscale*gd.getxInc()){
-			g2.drawLine(i, 0, i, getHeight());
+		for(double i=0; i<getWidth()-xAxis; i+=xscale*gd.getxInc()){
+			g2.drawLine((int) (xAxis+i), 0, (int) (xAxis+i), getHeight());
 		}
-		for(int i=0; i<getHeight(); i+=yscale*gd.getyInc()){
-			g2.drawLine(0, i, getWidth(), i);
+		for(double i=0; i<xAxis; i+=xscale*gd.getxInc()){
+			g2.drawLine((int) (xAxis-i), 0, (int) (xAxis-i), getHeight());
 		}
-		
-		//Graphs the functions
+		for(double i=0; i<gd.getyMax()*yscale; i+=yscale*gd.getyInc()){
+			g2.drawLine(0, (int) (yAxis+i), getWidth(), (int) (yAxis+i));
+		}
+		for(double i=0; i<yAxis; i+=yscale*gd.getyInc()){			
+			g2.drawLine(0, (int) (yAxis-i), getWidth(), (int) (yAxis-i));
+		}
+
 		ArrayList<Function> functions = graph.getFunctions();
 		for(int i=0; i<functions.size(); i++){
 			Function f = functions.get(i);
 			g2.setColor(f.getColor());
+			g2.setStroke(new BasicStroke(2));
 			for(double j=gd.getxMin(); j<gd.getxMax(); j+=gd.getInputInc()){
 				double y1 = f.evaluate(j);
 				double y2 = f.evaluate(j+gd.getInputInc());
 				if(y1>gd.getyMin() && y1<gd.getyMax() || y2>gd.getyMin() && y2<gd.getyMax()){
-					g2.drawLine((int) (j*xscale+getWidth()/2), (int) (getHeight()-y1*yscale-getHeight()/2), 
-							(int) ((j+gd.getInputInc())*xscale+getWidth()/2), (int) (getHeight()-y2*yscale-getHeight()/2));
+					g2.drawLine((int) (j*xscale+getWidth()/2)+xshift*xscale, 
+							(int) (getHeight()-y1*yscale-getHeight()/2)+yshift*yscale, 
+							(int) ((j+gd.getInputInc())*xscale+getWidth()/2)+xshift*xscale, 
+							(int) (getHeight()-y2*yscale-getHeight()/2)+yshift*yscale);
 				}
 			}
 		}
-		
+
 	}
-	
+
+	public void addKeyListener(KeyListener listener)
+	{
+		frame.addKeyListener(listener);
+	}
+
+	public void shiftX(int dx){ xshift += dx; }
+	public void shiftY(int dy){ yshift += dy; }
+
+	public int getXShift(){ return xshift; }
+	public int getYShift(){ return yshift; }
+
 }
